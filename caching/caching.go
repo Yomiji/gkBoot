0ad9073942556d8bc7bfdc67dd35db2ac256ae62
Yomiji <keith.track@gmail.com>
2021-09-18 07:59:04 -0400

    updated readme; added comments

--- caching/caching.go
+++ caching/caching.go
@@ -12,14 +12,23 @@ import (
 
 var CacheSaveFailed = fmt.Errorf("cache failed to save")
 
+// CacheableRequest
+//
+// Any object implementing CacheKey can represent a cache key for the purposes of a gkBoot cache
 type CacheableRequest interface {
 	CacheKey() string
 }
 
-type TTLCacheableRequest interface {
-	CacheTimeToLive() time.Duration
+// TimedCache
+//
+// Any object implementing CacheDuration will be a timed cache
+type TimedCache interface {
+	CacheDuration() time.Duration
 }
 
+// RequestCache
+//
+// Any object implementing Get and Put of this interface will qualify for being a RequestCache
 type RequestCache interface {
 	Get(ctx context.Context, key string) (interface{}, error)
 	Put(ctx context.Context, key string, value interface{}, ttl time.Duration) (interface{}, error)
@@ -46,8 +55,8 @@ func (g gkCache) Execute(ctx context.Context, request interface{}) (response int
 				return res, err2
 			}
 			var ttl time.Duration = 0
-			if timeable, ok := request.(TTLCacheableRequest); ok {
-				ttl = timeable.CacheTimeToLive()
+			if timeable, ok := request.(TimedCache); ok {
+				ttl = timeable.CacheDuration()
 			}
 			_, err = g.cache.Put(ctx, cacheable.CacheKey(), res, ttl)
 			if err != nil {
@@ -63,6 +72,10 @@ func (g gkCache) Execute(ctx context.Context, request interface{}) (response int
 	return g.next.Execute(ctx, request)
 }
 
+// WithCache
+//
+// Use to enable caching throughout the app. Cache will only be applied where the requests implement
+// at least CacheableRequest interface
 func WithCache(cache RequestCache) config.GkBootOption {
 	return func(config *config.BootConfig) {
 		config.ServiceWrappers = append(config.ServiceWrappers, func(srv service.Service) service.Service {
